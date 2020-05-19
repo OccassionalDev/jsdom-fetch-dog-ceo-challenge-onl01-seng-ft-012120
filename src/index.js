0eb17c8b7fc80f4d921995b98f014feaca4bc1bb
Erica Schimmel <ericaschimmel01@gmail.com>
2020-05-19 19:42:01 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ function loadImages() {
   const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
   
   fetch(imgUrl)
-    .then(resp=> resp.json())
-    .then(result => {
-      result.message.forEach(image => addImage(image))
+    .then(res=> res.json())
+    .then(results => {
+      results.message.forEach(image => addImage(image))
     });
 }
