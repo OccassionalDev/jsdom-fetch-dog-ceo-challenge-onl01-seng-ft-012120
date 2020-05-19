89d260ff79a6fd0e3556aba58109512593e503d4
Erica Schimmel <ericaschimmel01@gmail.com>
2020-05-19 19:39:18 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ console.log('%c HI', 'color: firebrick');
 
 // Challenge 1
 
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
-
 function loadImages() {
-  
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
 }
